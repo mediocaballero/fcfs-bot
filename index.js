136cd2540889112fe5b84a0e8d69a9a58e0a3e65
MidKnight <mediocaballero@gmail.com>
2020-07-18 23:57:16 +0000

    Adding express healthcheck and dotenv

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// ....... other express endpoints as per your project
+app.get('/', function (req, res) {
+  res.send('Bot activo!');
+});
+
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
 const FCFSClient = require('./src/fcfsclient');
 
 const client = new FCFSClient();
